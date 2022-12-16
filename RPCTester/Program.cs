cbe5828771928c5f085e47b2dda4ace4fb29355a
salehmu <ghd@keemail.me>
2022-12-16 20:52:10 +0200

    Remove unneeded project files

--- RPCTester/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using RPCHandler;
-using Sijl;
-
-Console.WriteLine("Hello, World!");
-var t = new SijlHandler();
-Console.WriteLine(t.Client.Register(new NewUserRequest()));
