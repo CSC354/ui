c6c0690c617ee7f4438d099de6073735f0cb5ba1
salehmu <ghd@keemail.me>
2022-12-24 02:12:32 +0200

    Update with new error handling

--- UI/Helpers/ErrorMessageHelper.cs
+++ UI/Helpers/ErrorMessageHelper.cs
@@ -1,39 +1,40 @@
-using Sijl;
+using Perrors;
 
 namespace UI.Helpers;
 
 public static class ErrorMessageHelper
 {
-    public static string BuildError(Err err)
+    public static string BuildError(int n)
     {
         var message = string.Empty;
+        var err = (Errors) n;
         const string please = "Please enter a valid ";
         switch (err)
         {
-            case Err.Ok:
+            case Errors.Ok:
                 break;
-            case Err.GeneratingToken:
+            case Errors.GeneratingToken:
                 message = "Something wrong happened";
                 break;
-            case Err.InvalidFirstName:
+            case Errors.InvalidFirstName:
                 message = please + "first name";
                 break;
-            case Err.InvalidLastName:
+            case Errors.InvalidLastName:
                 message = please + "last name";
                 break;
-            case Err.InvalidEmail:
+            case Errors.InvalidEmail:
                 message = please + "email address";
                 break;
-            case Err.InvalidAge:
+            case Errors.InvalidAge:
                 message = please + "age";
                 break;
-            case Err.InvalidUsername:
+            case Errors.InvalidUsername:
                 message = please + "username";
                 break;
-            case Err.AlreadyUsedUsername:
+            case Errors.AlreadyUsedUsername:
                 message = $"The username is already used, try to login instead";
                 break;
-            case Err.InvalidPassword:
+            case Errors.InvalidPassword:
                 message =
                     "The password you used is very weak, please add more special characters and different letter cases";
                 break;
--- UI/Program.cs
+++ UI/Program.cs
@@ -6,17 +6,16 @@ using Sijl;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
-
 builder.Services.AddServerSideBlazor();
-
-
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddBlazoredSessionStorage();
 
+const string sijl = "http://sijl:8000";
+const string discuss = "http://discuss:8000";
+
 builder.Services.AddSingleton(_ =>
 {
-    const string url = "172.21.0.5:8000";
-    var uriBuilder = new UriBuilder(url);
+    var uriBuilder = new UriBuilder(sijl);
     var channel = GrpcChannel.ForAddress(uriBuilder.Uri);
     return new Sijl.Sijl.SijlClient(channel);
 });
@@ -24,11 +23,18 @@ builder.Services.AddSingleton(_ =>
 
 builder.Services.AddSingleton(_ =>
 {
-    const string url = "172.21.0.5:8000";
-    var uriBuilder = new UriBuilder(url);
+    var uriBuilder = new UriBuilder(sijl);
     var channel = GrpcChannel.ForAddress(uriBuilder.Uri);
     return new Profile.ProfileClient(channel);
 });
+
+builder.Services.AddSingleton(_ =>
+    {
+        var uriBuilder = new UriBuilder(discuss);
+        var channel = GrpcChannel.ForAddress(uriBuilder.Uri);
+        return new Discuss.Discuss.DiscussClient(channel);
+    }
+);
 
 
 var app = builder.Build();
@@ -38,12 +44,8 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
-
 app.Run();
